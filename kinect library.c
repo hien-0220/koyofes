10796a8f6d225f60dfc3bcb45f167de001e90883
amanara46 <outatime88@outlook.jp>
2017-05-25 22:13:26 +0900

    キネクトライブラリーに関する情報Fileを追加

---
+++ kinect library.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    /*
+    キネクトライブラリー一覧
+    http://www.kinect-wiki.info/Kinect%E3%83%A9%E3%82%A4%E3%83%96%E3%83%A9%E3%83%AA
+    キネクトライブラリー追加
+    http://www.d-improvement.jp/learning/processing/libraries/kinect.html
+    */
+    return 0;
+}
